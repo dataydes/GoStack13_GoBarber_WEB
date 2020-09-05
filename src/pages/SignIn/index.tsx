8830138bf71b9117ae0bf96ce4396bebad00a138
Daniel Ataydes <daniel_ataydes@msn.com>
2020-09-05 19:46:39 -0300

    Criada a api de conexão com back-end

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,50 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import {FormHandles} from '@unform/core';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationErrors';
+import { AuthContext } from '../../context/AuthContext';
+
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const handleSubmit = useCallback(async (data: object) => {
-        try {
-            formRef.current?.setErrors({});
-            const schema = Yup.object().shape({                
-                email: Yup.string().required('Email obrigatório').email(),
-                password: Yup.string().required('Senha obrigatória'),
-            });
-            await schema.validate(data, {
-                abortEarly: false,
-            });
-        } catch (err) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
-        }
-    }, []);
+    const { signIn } = useContext(AuthContext);
+
+    const handleSubmit = useCallback(
+        async (data: SignInFormData) => {
+            try {
+                formRef.current?.setErrors({});
+                const schema = Yup.object().shape({
+                    email: Yup.string().required('Email obrigatório').email(),
+                    password: Yup.string().required('Senha obrigatória').min(6, 'Senha não confere.'),
+                });
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+                signIn({
+                    email: data.email,
+                    password: data.password,
+                });
+            } catch (err) {
+                const errors = getValidationErrors(err);
+                formRef.current?.setErrors(errors);
+            }
+        },
+        [signIn],
+    );
+
     return (
         <Container>
             <Content>
